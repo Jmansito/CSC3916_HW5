e1b7ee3fa2ad6a81dfce64029479603cc237075f
Josh Mansito <joshua.mansito@ucdenver.edu>
2021-03-31 16:33:16 -0600

    changed email to username in reg and signin

--- src/components/movieheader.js
+++ src/components/movieheader.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import {Navbar, NavItem, Nav} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
-import {connect} from "react-redux";
-import { withRouter } from "react-router-dom";
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom';
 import {logoutUser} from "../actions/authActions";
 
 class MovieHeader extends Component {
-
-    logout(){
+    logout() {
         this.props.dispatch(logoutUser());
     }
 
@@ -17,14 +16,14 @@ class MovieHeader extends Component {
                 <Navbar>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            Movie App
+                        Movie App
                         </Navbar.Brand>
                     </Navbar.Header>
                     <Nav>
                         <LinkContainer to="/movielist">
-                            <NavItem eventKey={1} disabled={!this.props.loggedIn}>Movie List </NavItem>
+                            <NavItem eventKey={1} disabled={!this.props.loggedIn}>Movie List</NavItem>
                         </LinkContainer>
-                        <LinkContainer to={'/movie/'+ (this.props.selectedMovie ? this.props.selectedMovie._id : '')}>
+                        <LinkContainer to={'/movie' + (this.props.selectedMovie ? this.props.selectedMovie._id : '')}>
                             <NavItem eventKey={2} disabled={!this.props.loggedIn}>Movie Detail</NavItem>
                         </LinkContainer>
                         <LinkContainer to="/signin">
@@ -32,20 +31,16 @@ class MovieHeader extends Component {
                         </LinkContainer>
                     </Nav>
                 </Navbar>
-                <header className="App-header">
-                    <h1 className="App-title">{(this.props.selectedMovie ? this.props.selectedMovie.title : '')}</h1>
-                </header>
             </div>
-
-        );
+        )
     }
 }
 
 const mapStateToProps = state => {
     return {
-        loggedIn: state.auth.loggedIn,
-        username: state.auth.username,
-        selectedMovie: ''//state.movie.selectedMovie,
+        loggedIn : state.auth.loggedIn,
+        username : state.auth.username,
+        selectedMovie: ''
     }
 }
 
